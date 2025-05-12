a96acbe49183d9e679e2d050370d89e91a83d571
kayquelopes <kaiquelopes.601@gmail.com>
2025-05-11 21:47:19 -0300

    feat: add code

---
+++ lista2/questao_31Fatura/demoFatura.java
@@ -0,0 +1,8 @@
+package lista2.questao_31Fatura;
+
+public class demoFatura {
+    public static void main(String[] args) {
+        Fatura fatura1 = new Fatura(1001, "Uma caixa de Ovos", 3, 10.0);
+        fatura1.imprimeFatura();
+    }
+}
